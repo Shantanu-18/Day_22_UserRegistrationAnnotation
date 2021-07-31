bfd9f737c935ea0f557571d1f1cfc115c312a405
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-01 00:11:14 +0530

    [Shantanu] Added : Annotation for email.

--- Day_22_UserRegistrationAnnotation/UserRegistration.cs
+++ Day_22_UserRegistrationAnnotation/UserRegistration.cs
@@ -11,8 +11,13 @@ namespace Day_22_UserRegistrationAnnotation
         [StringLength(100,MinimumLength =2,ErrorMessage="Name should contain minimum two characters.")]
         [DataType(DataType.Text)]
         public string firstName { get; set; }
+
         [StringLength(100, MinimumLength = 2, ErrorMessage = "Name should contain minimum two characters.")]
         [DataType(DataType.Text)]
         public string lastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string email { get; set; }
     }
 }
