5784fde084bd9d15bddfc943d932696517ef4f67
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-31 23:43:14 +0530

    [Shantanu] Added : Annotation for last name.

--- Day_22_UserRegistrationAnnotation/UserRegistration.cs
+++ Day_22_UserRegistrationAnnotation/UserRegistration.cs
@@ -10,6 +10,9 @@ namespace Day_22_UserRegistrationAnnotation
         [Required(ErrorMessage = "Users {0} is required")]
         [StringLength(100,MinimumLength =2,ErrorMessage="Name should contain minimum two characters.")]
         [DataType(DataType.Text)]
-        public string firstName { get; set; } 
+        public string firstName { get; set; }
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name should contain minimum two characters.")]
+        [DataType(DataType.Text)]
+        public string lastName { get; set; }
     }
 }
