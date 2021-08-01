6a7633471672d2f0c97e84e36b6fe740f5e4da68
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-01 20:51:56 +0530

    [Shantanu] Added : Annotation for password validation.

--- Day_22_UserRegistrationAnnotation/UserRegistration.cs
+++ Day_22_UserRegistrationAnnotation/UserRegistration.cs
@@ -8,11 +8,13 @@ namespace Day_22_UserRegistrationAnnotation
     class UserRegistration
     {
         [Required(ErrorMessage = "Users {0} is required")]
-        [StringLength(100,MinimumLength =2,ErrorMessage="Name should contain minimum two characters.")]
+        [RegularExpression("^[A-Z][a-z]{2,}$", ErrorMessage = "Name should start with capital letters and" +
+                                                                  "must have minimum 2 characters.")]
         [DataType(DataType.Text)]
         public string firstName { get; set; }
 
-        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name should contain minimum two characters.")]
+        [RegularExpression("^[A-Z][a-z]{2,}$", ErrorMessage = "Name should start with capital letters and" +
+                                                                    "must have minimum 2 characters.")]
         [DataType(DataType.Text)]
         public string lastName { get; set; }
 
@@ -24,7 +26,8 @@ namespace Day_22_UserRegistrationAnnotation
         [Phone]
         public string phoneNumber { get; set; }
 
-        [StringLength(15,MinimumLength =8,ErrorMessage = "Password should be between 8 to 15 characters")]
+        [RegularExpression("^((?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_.])(?=.{8,}))",ErrorMessage = "Password should " +
+                            "contain minimum 8 character atleast one upper case character and atleast one numeric value")]
         [DataType(DataType.Password)]
         public string password { get; set; }
     }
